e93f2c8f03d15969c1d8e6ee414ba6f53a3842db
Aleksandr Beloborodov <a.beloborodov@leadgid.ru>
2022-10-18 08:25:26 +0300

    fix

--- src/DependencyInjection/RunConsoleExtension.php
+++ src/DependencyInjection/RunConsoleExtension.php
@@ -9,7 +9,7 @@ use Sabel\RunConsoleBundle\Controller\MainController;
 use Sabel\RunConsoleBundle\Service\ConsoleRunner;
 use Sabel\RunConsoleBundle\Service\Responder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
@@ -41,7 +41,7 @@ final class RunConsoleExtension extends Extension
             ->register('run_console.runner', ConsoleRunner::class)
             ->setArgument('$commands', $container->getParameter('run_console.commands'))
             ->setArgument('$kernel', new Reference(KernelInterface::class))
-            ->setArgument('$output', new Reference(BufferedOutput::class))
+            ->setArgument('$output', new Reference(OutputInterface::class))
             ->setAutoconfigured(true)
             ->setPublic(true)
             ->setAutowired(true);
